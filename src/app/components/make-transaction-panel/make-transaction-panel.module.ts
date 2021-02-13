175c85f0ad36bb435f42c2464df9199b9a4c1648
Nathan Jones <nathanmjones1@gmail.com>
2021-02-13 07:23:46 +0000

    Added card actions to the transactions panel

--- src/app/components/make-transaction-panel/make-transaction-panel.module.ts
+++ src/app/components/make-transaction-panel/make-transaction-panel.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MakeTransactionPanelComponent } from './make-transaction-panel.component';
 import { CardModule } from '../card/card.module';
+import { BbUIModule } from 'src/vendor/bb-ui/bb-ui.module';
 const exports = [MakeTransactionPanelComponent];
 @NgModule({
   exports,
   declarations: exports,
-  imports: [CommonModule, CardModule],
+  imports: [CommonModule, CardModule, BbUIModule],
 })
 export class MakeTransactionPanelModule {}
