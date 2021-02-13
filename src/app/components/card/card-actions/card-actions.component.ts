bd0978fccdb693165701c69012df9544b5173d32
Nathan Jones <nathanmjones1@gmail.com>
2021-02-13 07:23:19 +0000

    Added card actions component

---
+++ src/app/components/card/card-actions/card-actions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-actions',
+  templateUrl: './card-actions.component.html',
+  styleUrls: ['./card-actions.component.scss'],
+})
+export class CardActionsComponent {}
--- src/app/components/card/card.module.ts
+++ src/app/components/card/card.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
 import { CardHeaderComponent } from './card-header/card-header.component';
+import { CardActionsComponent } from './card-actions/card-actions.component';
 
-const exports = [CardComponent, CardHeaderComponent];
+const exports = [CardComponent, CardHeaderComponent, CardActionsComponent];
 
 @NgModule({
   exports,
